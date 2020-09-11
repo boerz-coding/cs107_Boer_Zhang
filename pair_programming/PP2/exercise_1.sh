24d458abc3033dd43132ed35b0a928d702d61f56
boerz-coding <boerzhang@g.harvard.edu>
2020-09-11 00:10:09 +0000

    testexercise_1

--- pair_programming/PP2/exercise_1.sh
+++ pair_programming/PP2/exercise_1.sh
@@ -4,15 +4,17 @@ echo "$name"
 git add $name
 git status
 read -r -p "Continue?(Y/N)" continue_yn
-if [ "continue_yn"="Y"]; then
+if [ "$continue_yn" = "Y" ]; then
+	echo Yes
 	read -r -p "Type your commit message:" commit_msg
-elif [ "continue_yn"="N"]; then
+	echo "$commit_msg"
+else
 	exit 1
 fi
 git commit -m $commit_msg
 git status
 read -r -p "Continue?(Y/N)" continue_yn
-if ["continue_yn"="Y"]; then
+if ["$continue_yn"="Y"]; then
 	git push
 fi
 
